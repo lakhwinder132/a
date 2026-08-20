3e8056a755d3d41678144df6e0e6022863d84d8c
lakhwinder132 <lakhwinderjhim@gmail.com>
2026-08-20 11:29:48 +0000

    dfadsfadsf

---
+++ d.js
@@ -0,0 +1,22 @@
+import express from "express";
+import os from "os";
+
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send("Hello World");
+});
+
+app.get("/cpu",(req,res)=>{
+    for(let i=0;i<1000000000;i++){
+        Math.random();
+    }
+    res.send('hello world');
+});
+app.get("/host",(req,res)=>{
+    res.send(os.hostname());
+});
+
+app.listen(3001,()=>{
+    console.log("listening on pot 3000");
+});
